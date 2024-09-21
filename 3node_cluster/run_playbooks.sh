f3e8c59965a6c0451595c6ab8a10ee581043b197
Avignan Chatterjee <again.1550@gmail.com>
2024-09-20 23:15:16 -0700

    Organize code.

--- 3node_cluster/run_playbooks.sh
+++ 3node_cluster/run_playbooks.sh
@@ -4,18 +4,18 @@ echo "Create vagrant cluster"
 vagrant up
 
 echo "Setup password less ssh with virtual hosts"
-./set_passwd_less_ssh.sh
+./utils/set_passwd_less_ssh.sh
 
 echo "Setup k8s on cluster" 
-ansible-playbook -i hosts.ini k8s-setup.yml
-ansible-playbook -i hosts.ini k8s-master.yml
-ansible-playbook -i hosts.ini k8s-workers.yml
-ansible-playbook -i hosts.ini install-helm.yml
+ansible-playbook -i ansible_playbooks/hosts.ini ansible_playbooks/k8s-setup.yml
+ansible-playbook -i ansible_playbooks/hosts.ini ansible_playbooks/k8s-master.yml
+ansible-playbook -i ansible_playbooks/hosts.ini ansible_playbooks/k8s-workers.yml
+ansible-playbook -i ansible_playbooks/hosts.ini ansible_playbooks/install-helm.yml
 
 # Get kubeconfig from master.
 echo "Setup kubeconfig"
 sshpass -pvagrant scp vagrant@192.168.1.101:/home/vagrant/.kube/config /tmp/kubeconfig
-source ./set_kubeconfig.sh
+source ./utils/set_kubeconfig.sh
 
 echo "Creating NATS cluster"
 pushd ./nats_cluster
