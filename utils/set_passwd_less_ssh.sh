f4ac15d0fa7f25ef0cf9eb7c05470820942cc802
Avignan Chatterjee <again.1550@gmail.com>
2024-11-09 23:13:52 -0800

    Fix kube-proxy and other kube-system pods from crashing. Following fix https://discuss.kubernetes.io/t/why-do-my-pods-keep-crashing/26558/12

--- utils/set_passwd_less_ssh.sh
+++ utils/set_passwd_less_ssh.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 for i in {1..3}; do sshpass -pvagrant ssh-copy-id -o "StrictHostKeyChecking no" vagrant@192.168.1.10$i; done
+#for i in `for i in {1..3}; do vagrant ssh node$i -c "ip addr show eth0 | grep 'inet '"; done | awk '{print $2}' | cut -d'/' -f1`; do sshpass -pvagrant ssh-copy-id -o "StrictHostKeyChecking no" vagrant@$i; done
