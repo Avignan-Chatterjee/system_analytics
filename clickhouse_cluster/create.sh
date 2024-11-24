b2daf9aa208e72c573a4e2f023ea49228f80065f
Avignan Chatterjee <again.1550@gmail.com>
2024-11-23 23:30:41 -0800

    Setup clickhouse DB.

---
+++ clickhouse_cluster/create.sh
@@ -0,0 +1,13 @@
+set -e
+
+echo "Creating clickhouse installation CRD"
+kubectl apply -f clickhouse-operator-install-bundle.yaml
+
+echo "Creating clickhouse namespace"
+kubectl apply -f clickhouse-namespace.yaml
+
+echo "Creating clickhous pvs"
+kubectl apply -f clickhouse-pv.yaml
+
+echo "Creating clickhous cluster"
+kubectl apply -f clickhouse-cluster.yaml
